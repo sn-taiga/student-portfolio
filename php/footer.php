147da96562c1632a00631297342e643116ad1fa3
taiga-sano <taiga-sano@cookpad.com>
2020-01-24 12:59:59 +0900

    add files

---
+++ php/footer.php
@@ -0,0 +1,14 @@
+<footer>
+		<nav>
+			<ul>
+				<li><a href="/" title="Top">Top</a></li>
+				<li><a href="works/" title="Works">Works</a></li>
+				<li><a href="about/" title="About">About</a></li>
+				<li><a href="contact/" title="Contact">Contact</a></li>
+			</ul>
+		</nav>
+		<p class="copyright">© 2015 TaigaSano</p>
+	</footer>
+	<script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="/js/lib.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
---
+++ php/meta.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<link rel="stylesheet" href="css/style.css">
+
+<meta name="copyright" content="Taiga Sano"/>
+<meta name="keywords" lang="ja" content="TaigaSano, Taiga Sano, 佐野大河, さのたいが"/>
+<meta name="description" lang="ja" content="在学中に制作した作品とその活動について紹介しています。研究、授業課題、自主制作、ハッカソンなどで制作した物の中からいくつか取り上げて載せました。"/>
+
+<meta property="og:title" content="TAIGA SANO - Portfolio Website">
+<meta property="og:type" content="website">
+<meta property="og:description" content="在学中に制作した作品とその活動について紹介しています。研究、授業課題、自主制作、ハッカソンなどで制作した物の中からいくつか取り上げて載せました。">
+<meta property="og:url" content="http://taigasano.com/">
+<meta property="og:image" content="http://taigasano.com/img/sns_share.jpg">
+<meta property="og:site_name" content="TAIGA SANO - Portfolio Website">
